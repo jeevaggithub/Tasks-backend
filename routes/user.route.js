de8109b2814b3ad3f7f32192f33a8dd89d040861
jeevaggithub <jeevagopal2000@gmail.com>
2023-08-24 13:18:21 +0530

    node changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,9 +3,30 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 
 app.use(cors());
+
+
+//Token Verification Middleware:
+
+function verifyToken(req, res, next) {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(403).json({ message: 'Token missing' });
+    }
+
+    jwt.verify(token, 'secret_key', (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Token invalid' });
+        }
+
+        req.userId = decoded.userId;
+        next();
+    });
+}
 
 //define the schema
 
@@ -80,9 +101,36 @@ app.post('/signin', async (req, res) => {
                 res.status(401).json({ message: 'Invalid password' });
             } else {
                 // If the email and password match, return the user details
-                res.status(200).json({ message: 'user authenticated' });
+                const token = jwt.sign({ userId: user._id }, 'secret_key', { expiresIn: '1h' });
+                res.status(200).json({ message: 'user authenticated', token });
                 // res.json(user);
             }
+        }
+    } catch (error) {
+        console.error('Error while signing in:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+// User Route
+app.post('/user', verifyToken, async (req, res) => {
+    try {
+        const { email } = req.body;
+
+        // Find the user with the provided email
+        const user = await Userchk.findOne({ email });
+
+        if (!user) {
+            // If no user is found, return an error response
+            res.status(401).json({ message: 'Invalid email' });
+        } else {
+            // Check if the password matches the one stored in the database
+
+            res.status(401).json({ User: user });
+
+            // If the email and password match, return the user details
+            // res.status(200).json({ message: 'user authenticated' });
+            // res.json(user);
+
         }
     } catch (error) {
         console.error('Error while signing in:', error);
